67aab559cbeb34131b1937d498c09dfc3969eeba
Dai Alqurni <dai@imlink.network>
2025-10-13 12:54:07 +0300

    fix role not found issue on database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 Base = declarative_base()
-engine = create_engine("postgresql://user:password@localhost:5432/recipe_db")
+engine = create_engine("postgresql://postgres@localhost:5432/recipe_db")
 SessionLocal = sessionmaker(bind=engine)
 
 
